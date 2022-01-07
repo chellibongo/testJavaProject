6d2f8d1ab1df035ca33169353be3d7e774280609
Chris Ellis <ce8958@gmail.com>
2022-01-07 12:24:51 +0000

    fizzbuzz complete

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        for(int i = 0; i < 100; i++) {
+            if((i % 3 == 0) && (i % 5 == 0)) {
+                System.out.println("FizzBuzz");
+            }
+            else if(i % 3 == 0) {
+                System.out.println("Fizz");
+            }
+            else if(i % 5 == 0) {
+                System.out.println("Buzz");
+            }
+            
+        }
     }
 }
