bdeb081e214f61625aa38f631f72fb5a0df62d86
Chris Ellis <ce8958@gmail.com>
2022-01-07 14:31:03 +0000

    added webcrawler

--- src/fizzBuzz.java
+++ src/fizzBuzz.java
@@ -1,4 +1,4 @@
-public class App {
+public class fizzBuzz {
     public static void main(String[] args) throws Exception {
         for(int i = 0; i < 100; i++) {
             if((i % 3 == 0) && (i % 5 == 0)) {
